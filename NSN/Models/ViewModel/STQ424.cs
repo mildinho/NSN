11cddb38e8a3e99522f23480bec4b5c83931160d
mildinho <62845578+mildinho@users.noreply.github.com>
2022-03-31 11:16:15 -0300

    Criação da ViewModel

---
+++ NSN/Models/ViewModel/STQ424.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSN.Models.ViewModel
+{
+    public class STQ424
+    {
+        public Stq ProdutoFilial;
+        public List<Stq> ProdutoGeral;
+            
+    }
+}
